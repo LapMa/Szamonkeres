9fa2e01edf7db787187d424471bdcadcd00e5d4f
Lapostyán Martin <lapostyanm@KKSZKI.LOCAL>
2022-12-01 13:18:14 +0100

    5 feladat kész

--- python/main.py
+++ python/main.py
@@ -3,12 +3,14 @@
 """
 nev = input('kérek egy vezetéknevet: ')
 nev2 = input('kérek egy kerszetnevet: ')
+
 print(f"Üdv, {nev} {nev2}")
 print(f"Üdv, {nev2} {nev}")"""
 
 #2Feladat
 """
 szam = int(input("Kéregy egy számot: "))
+
 print("A megelöző szám:", (szam - 1))
 print("A megelöző szám:", (szam + 1))"""
 
@@ -16,6 +18,7 @@ print("A megelöző szám:", (szam + 1))"""
 #3Feladat
 """
 szam = int(input("Kérek egy számot"))
+
 szam2 = int(input("Kérek egy számot"))
 print("A két szám összege:",(szam + szam2))
 print("A két szám különbsége:", (szam - szam2))
@@ -26,9 +29,18 @@ print("A két szám hányadosa:" (szam % szam2))"""
 """
 szam = int(input("Kérek egy számot"))
 szam2 = int(input("Kérek egy számot"))
+
 c =(2* szam + 3*szam2)
 d =(2* szam - 3*szam2)
+
 print("C áltozó:"(c))
 print("D változó: "(d))"""
 
 #5Feladat
+for szam in range(1,21):
+    if szam % 2 == 0:
+        print("Páros számok 1-20",szam,end=",")
+print()
+for szam2 in range(1,21):
+    if szam2 % 2 == 1:
+        print("Páratlan számok 1-20-ig",szam2,end=",")
